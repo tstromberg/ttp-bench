481f3a58d2cefa16aeb1f12acc3105dacccfd3f3
Thomas Stromberg <t+github@stromberg.org>
2022-03-05 07:23:10 -0800

    Omit -p from netcat -l

--- cmd/exec-netcat-listen/main.go
+++ cmd/exec-netcat-listen/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	c := exec.Command("nc", "-l", "-p", "12345")
+	c := exec.Command("nc", "-l", "12345")
 	log.Printf("running %s ...", c)
 	bs, err := c.CombinedOutput()
 	if err != nil {
