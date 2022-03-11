6b77711d112b9db3115549c240123a5e515c335d
Thomas Stromberg <t+github@chainguard.dev>
2022-03-10 17:12:55 -0800

    Add traitor vuln probe

---
+++ cmd/exec-traitor-vuln-probe/main.go
@@ -0,0 +1,16 @@
+//go:build linux
+
+// Simulates probing system for privilege escalation vulns
+package main
+
+import (
+	"log"
+
+	"github.com/tstromberg/ioc-bench/pkg/simulate"
+)
+
+func main() {
+	if err := simulate.Traitor(); err != nil {
+		log.Fatalf("exploit failed: %v", err)
+	}
+}
